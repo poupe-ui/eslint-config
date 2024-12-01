a6e474968307117bd383e71a5c7c9745398d625d
Alejandro Mery <amery@apptly.co>
2024-12-01 23:17:56 +0000

    feat: rename PropType<> to PropertyType<> to make unicorn happy
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
-import { PropType } from './utils';
+import { PropertyType } from './utils';
 
 import type { TypedFlatConfig } from 'eslint-config-unjs';
 
 export type { TypedFlatConfig } from 'eslint-config-unjs';
 
-export type Rules = PropType<TypedFlatConfig, 'rules'>;
+export type Rules = PropertyType<TypedFlatConfig, 'rules'>;
