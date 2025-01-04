3a05bb5505569bf626f249442c7397482ef2752b
Alejandro Mery <amery@apptly.co>
2025-01-04 19:42:11 +0000

    fix(nuxt): fix forNuxt() type
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -1,5 +1,7 @@
-import type { Config } from './core';
+import { Linter } from 'eslint';
 import { files, rules } from './configs';
+
+type Config = Linter.Config;
 
 export const forNuxt = (): Config[] => [
   {
